23aa1ee25d6aa02e3a33e716dfda6838925ebdc3
Ousmane Diallo <diallodev45@gmail.com>
2025-08-14 14:12:20 +0200

    enlevement du middleware d'authentification

--- routes/taches.route.js
+++ routes/taches.route.js
@@ -5,10 +5,10 @@ const auth = require("../middlewares/auth.midd")
 
 
 //la fonction pour enregistre une tache
-route.post("/",auth,tacheControllers.createTaches)
+route.post("/",tacheControllers.createTaches)
 
 // la route pour filter
-route.get("/",auth,tacheControllers.readTaches)
+route.get("/",tacheControllers.readTaches)
 
 //la route pour afficher une tache
 route.get("/:id",tacheControllers.readTache)
